d60f7cffc93e702e4651d94c9b56338c78954c73
beatrizac03 <beatrizaguiarc14@gmail.com>
2025-06-02 19:15:18 -0300

    deploy: allow all origins

--- src/main/java/com/api/cuida/infra/security/SecurityConfig.java
+++ src/main/java/com/api/cuida/infra/security/SecurityConfig.java
@@ -53,10 +53,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
